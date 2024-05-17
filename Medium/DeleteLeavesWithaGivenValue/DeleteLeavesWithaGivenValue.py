b86b84e4018ef8353fec2688d9652290223d52e5
Zaid Omer <zaidomerone@gmail.com>
2024-05-16 23:45:20 -0400

    Today's daily and an extra easy problem

---
+++ Medium/DeleteLeavesWithaGivenValue/DeleteLeavesWithaGivenValue.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/delete-leaves-with-a-given-value/?envType=daily-question&envId=2024-05-17
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution:
+    # def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+    #     if root.right:
+    #         self.removeLeafNodes(root.right, target)
+    #     if root.left:
+    #         self.removeLeafNodes(root.left, target)
+    #     if root.left and root.left.val==target and not root.left.left and not root.left.right:
+    #         root.left = None
+    #     if root.right and root.right.val==target and not root.right.left and not root.right.right:
+    #         root.right = None
+    #     if not root or (root.val==target and not root.left and not root.right):
+    #         return None
+
+    #     return root
+
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        if not root:
+            return
+
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+
+        if not root.left and not root.right and root.val == target:
+            return
+        return root 
+
+        
